49f346b015a9ce2da7f319c2b646b974aa35d055
Pedro Neto <pedromneto97@gmail.com>
2019-04-09 20:02:32 -0300

    Residences list

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -18,9 +18,56 @@ var server = [
     'Sign contract for "What are conference organizers afraid of?"'
 ];
 
+let residences = [
+    {
+        "_id": "5c8026f9dd2745212057540e",
+        "type": {
+            "_id": "5c7f2393dd27450ffc4bcbea",
+            "type": "Apartment"
+        },
+        "name": "Casa em Itapeva",
+        "address": {
+            "postal_code": {
+                "_id": "5c7f1ed8dd27453334c151b5",
+                "postal_code": "18405060",
+                "city": "Itapeva",
+                "province": "São Paulo",
+                "country": "Brazil",
+            },
+            "district": "Jardim Ferrari",
+            "street": "7 de Setembro",
+            "number": 176
+        },
+        "icon": "home"
+    },
+    {
+        "_id": "5c899fbfdd274507687ee730",
+        "type": {
+            "_id": "5c7f2393dd27450ffc4bcbea",
+            "type": "Apartment"
+        },
+        "name": "Casa em Itapeva",
+        "address": {
+            "postal_code": {
+                "_id": "5c86fc32dd274504dc012610",
+                "postal_code": "84026000",
+                "city": "Ponta Grossa",
+                "province": "Paraná",
+                "country": "Brazil",
+            },
+            "district": "Uvaranas",
+            "street": "Ana Rita",
+            "number": 296,
+            "complement": "Apartamento 21"
+        },
+        "icon": "home"
+    }
+];
+
 module.exports = {
     // these 3 are used to create the tasks lists in TasksCard - Dashboard view
     bugs,
     website,
-    server
+    server,
+    residences
 };
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -7,7 +7,6 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
 import Update from "@material-ui/icons/Update";
@@ -23,29 +22,35 @@ import GridContainer from "components/Grid/GridContainer.jsx";
 import Table from "components/Table/Table.jsx";
 import Tasks from "components/Tasks/Tasks.jsx";
 import CustomTabs from "components/CustomTabs/CustomTabs.jsx";
-import Danger from "components/Typography/Danger.jsx";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 
-import {bugs, server, website} from "variables/general.jsx";
+import Residence from "components/Residences/Residence.jsx";
+
+import {bugs, residences, server, website} from "variables/general.jsx";
 
 import {completedTasksChart, dailySalesChart, emailsSubscriptionChart} from "variables/charts.jsx";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
 
+function ResidenceList(props) {
+    const {
+        residences,
+        classes
+    } = props;
+    return (residences.map((residence) =>
+        <GridItem xs={12} sm={6} md={3}>
+            <Residence classes={classes} residence={residence}/>
+        </GridItem>
+    ));
+}
+
 class Dashboard extends React.Component {
     state = {
         value: 0
-    };
-    handleChange = (event, value) => {
-        this.setState({value});
-    };
-
-    handleChangeIndex = index => {
-        this.setState({value: index});
     };
 
     render() {
@@ -53,29 +58,7 @@ class Dashboard extends React.Component {
         return (
             <div>
                 <GridContainer>
-                    <GridItem xs={12} sm={6} md={3}>
-                        <Card>
-                            <CardHeader color="warning" stats icon>
-                                <CardIcon color="warning">
-                                    <Icon>content_copy</Icon>
-                                </CardIcon>
-                                <p className={classes.cardCategory}>Used Space</p>
-                                <h3 className={classes.cardTitle}>
-                                    49/50 <small>GB</small>
-                                </h3>
-                            </CardHeader>
-                            <CardFooter stats>
-                                <div className={classes.stats}>
-                                    <Danger>
-                                        <Warning/>
-                                    </Danger>
-                                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                                        Get more space
-                                    </a>
-                                </div>
-                            </CardFooter>
-                        </Card>
-                    </GridItem>
+                    <ResidenceList classes={classes} residences={residences}/>
                     <GridItem xs={12} sm={6} md={3}>
                         <Card>
                             <CardHeader color="success" stats icon>
