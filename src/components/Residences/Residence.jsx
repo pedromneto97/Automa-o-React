8bf9217cd619cddc01ab65d206e868345bbe1df6
pedromneto97 <pedromneto97@gmail.com>
2019-04-11 19:52:54 -0300

    Checks if has city

--- src/components/Residences/Residence.jsx
+++ src/components/Residences/Residence.jsx
@@ -11,34 +11,34 @@ import CardIcon from "components/Card/CardIcon.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 
 class Residence extends React.Component {
-    render() {
-        const {
-            classes,
-            residence
-        } = this.props;
-        return (
-            <Card>
-                <CardHeader color="success" stats icon>
-                    <CardIcon color="success">
-                        <Icon>{residence.icon}</Icon>
-                    </CardIcon>
-                    <p className={classes.cardCategory}>{residence.type.type}</p>
-                    <h3 className={classes.cardTitle}>{residence.name}</h3>
-                </CardHeader>
-                <CardFooter stats>
-                    <div className={classes.stats}>
-                        <LocationCity/>
-                        {residence.address.street}, {residence.address.number}, {residence.address.district} - {residence.address.postal_code.city}
-                    </div>
-                </CardFooter>
-            </Card>
-        );
-    }
+  render() {
+    const {
+      classes,
+      residence
+    } = this.props;
+    return (
+      <Card>
+        <CardHeader color="success" stats icon>
+          <CardIcon color="success">
+            <Icon>{residence.icon}</Icon>
+          </CardIcon>
+          <p className={classes.cardCategory}>{residence.type.type}</p>
+          <h3 className={classes.cardTitle}>{residence.name}</h3>
+        </CardHeader>
+        <CardFooter stats>
+          <div className={classes.stats}>
+            <LocationCity/>
+            {residence.address.street}, {residence.address.number}, {residence.address.district} {residence.address.postal_code.city ? "- " + residence.address.postal_code.city : ""}
+          </div>
+        </CardFooter>
+      </Card>
+    );
+  }
 }
 
 Residence.propTypes = {
-    classes: PropTypes.object.isRequired,
-    residence: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  residence: PropTypes.object.isRequired
 };
 
 export default Residence;
