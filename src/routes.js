16019387e59b1174d46d2e570e52735a9e8b9ff5
Pedro Neto <pedromneto97@gmail.com>
2019-04-14 15:40:25 -0300

    Residence and room views

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import Notifications from "@material-ui/icons/Notifications";
 import Unarchive from "@material-ui/icons/Unarchive";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import Residence from "views/Residence/Residence.jsx";
+import Room from "views/Room/Room.jsx";
 import UserProfile from "views/UserProfile/UserProfile.jsx";
 import TableList from "views/TableList/TableList.jsx";
 import Typography from "views/Typography/Typography.jsx";
@@ -23,56 +25,78 @@ const dashboardRoutes = [
         name: "Dashboard",
         icon: Dashboard,
         component: DashboardPage,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
+    },
+    {
+        path: "/residence/:alias",
+        name: "Residence",
+        component: Residence,
+        layout: "/admin",
+        invisible: true
+    },
+    {
+        path: "/room/:alias",
+        name: "Room",
+        component: Room,
+        layout: "/admin",
+        invisible: true
     },
     {
         path: "/user",
         name: "User Profile",
         icon: Person,
         component: UserProfile,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/table",
         name: "Table List",
         icon: "content_paste",
         component: TableList,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/typography",
         name: "Typography",
         icon: LibraryBooks,
         component: Typography,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/icons",
         name: "Icons",
         icon: BubbleChart,
         component: Icons,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/maps",
         name: "Maps",
         icon: LocationOn,
         component: Maps,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/notifications",
         name: "Notifications",
         icon: Notifications,
         component: NotificationsPage,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
     {
         path: "/upgrade-to-pro",
         name: "Upgrade To PRO",
         icon: Unarchive,
         component: UpgradeToPro,
-        layout: "/admin"
+        layout: "/admin",
+        invisible: false
     },
 ];
 
