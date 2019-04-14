6b0cf238ed802b6db148adf4e8e19b727e2d72b9
Pedro Neto <pedromneto97@gmail.com>
2019-04-14 15:42:27 -0300

    Link

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 // nodejs library to set properties for components
 import PropTypes from "prop-types";
 
@@ -14,36 +14,36 @@ import CardFooter from "components/Card/CardFooter.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 
 class Room extends React.Component {
-  render() {
-    const {
-      classes,
-      room
-    } = this.props;
-    return (
-      <Card>
-        <CardHeader color="success" stats icon>
-          <CardIcon color="info">
-            <Icon>{room.icon ? room.icon : room.type.icon}</Icon>
-          </CardIcon>
-          <p className={classes.cardCategory}>{room.type.type}</p>
-          <h3 className={classes.cardTitle}>{room.name}</h3>
-        </CardHeader>
-        <CardFooter stats>
-          <div className={classes.stats}>
-            <AccessTime/>
-          </div>
-          <Link to={"/admin/"}>
-            <Button color="info" size="sm">More info</Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    );
-  }
+    render() {
+        const {
+            classes,
+            room
+        } = this.props;
+        return (
+            <Card>
+                <CardHeader color="success" stats icon>
+                    <CardIcon color="info">
+                        <Icon>{room.icon ? room.icon : room.type.icon}</Icon>
+                    </CardIcon>
+                    <p className={classes.cardCategory}>{room.type.type}</p>
+                    <h3 className={classes.cardTitle}>{room.name}</h3>
+                </CardHeader>
+                <CardFooter stats>
+                    <div className={classes.stats}>
+                        <AccessTime/>
+                    </div>
+                    <Link to={"/admin/room/" + room.alias}>
+                        <Button color="info" size="sm">Check room</Button>
+                    </Link>
+                </CardFooter>
+            </Card>
+        );
+    }
 }
 
 Room.propTypes = {
-  classes: PropTypes.object.isRequired,
-  residence: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    room: PropTypes.object.isRequired
 };
 
 export default Room;
