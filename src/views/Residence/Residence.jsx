f9b5fa7c21009b5412a72113e96b61f6c8bb3ad4
Pedro Neto <pedromneto97@gmail.com>
2019-04-14 15:41:26 -0300

    Correct component

--- src/views/Residence/Residence.jsx
+++ src/views/Residence/Residence.jsx
@@ -6,85 +6,88 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 // Room module
-import Residence from "components/Residences/Room.jsx";
+import Room from "components/Room/Room.jsx";
 // Crossbar
-import { Connection } from "autobahn";
+import {Connection} from "autobahn";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
 
 function RoomsList(props) {
-  const {
-    rooms,
-    classes
-  } = props;
-  return (rooms.map((room) =>
-    <GridItem xs={12} sm={6} md={3} key={room._id.$oid}>
-      <Residence classes={classes} room={room}/>
-    </GridItem>
-  ));
+    const {
+        rooms,
+        classes
+    } = props;
+    return (rooms.map((room) =>
+        <GridItem xs={12} sm={6} md={3} key={room._id.$oid}>
+            <Room classes={classes} room={room}/>
+        </GridItem>
+    ));
 }
 
 class Residence extends React.Component {
-  constructor(props) {
-    super(props);
-    let ws_uri;
-    if (document.location.hostname === "localhost") {
-      ws_uri = {
-        transports: [
-          {
-            "type": "websocket",
-            "url": "ws://crossbar-pedro.herokuapp.com/ws"
-          }
-        ]
-      };
-    } else {
-      ws_uri = {
-        url: (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" + document.location.host + "/ws"
-      };
+    constructor(props) {
+        super(props);
+        let ws_uri;
+        if (document.location.hostname === "localhost") {
+            ws_uri = {
+                transports: [
+                    {
+                        "type": "websocket",
+                        "url": "ws://crossbar-pedro.herokuapp.com/ws"
+                    }
+                ]
+            };
+        } else {
+            ws_uri = {
+                url: (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" + document.location.host + "/ws"
+            };
+        }
+        ws_uri["realm"] = "realm1";
+        this.state = {
+            residence: {
+                rooms: []
+            },
+            connection: new Connection(ws_uri),
+            alias: props.match.params.alias
+        };
+        this.state.connection.onopen = function (session, details) {
+            console.info("Aberto!");
+        };
+        this.state.connection.open();
+        this.getRooms = this.getRooms.bind(this);
     }
-    ws_uri["realm"] = "realm1";
-    this.state = {
-      rooms: [],
-      connection: new Connection(ws_uri)
-    };
-    this.state.connection.onopen = function(session, details) {
-      console.info("Aberto!");
-    };
-    this.state.connection.open();
-    this.getRooms = this.getRooms.bind(this);
-  }
 
-  componentDidMount(): void {
-    setTimeout(this.getRooms, 3000);
-  }
+    componentDidMount(): void {
+        setTimeout(this.getRooms, 3000);
+    }
 
-  getRooms() {
-    this.state.connection.session.call("com.herokuapp.crossbar-pedro.residence.alias", ["5c7f1130dd27452be8f16adc"])
-      .then(function(res) {
-        this.setState({
-          rooms: JSON.parse(res)
-        });
-      }.bind(this))
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
+    getRooms() {
+        this.state.connection.session.call("com.herokuapp.crossbar-pedro.residence.alias", [this.state.alias])
+            .then(function (res) {
+                this.setState({
+                    residence: JSON.parse(res)
+                });
+            }.bind(this))
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <GridContainer>
-          <RoomsList classes={classes} rooms={this.state.rooms}/>
-        </GridContainer>
-      </div>
-    );
-  }
+    render() {
+        const {classes} = this.props;
+        return (
+            <div>
+                <GridContainer>
+                    <RoomsList classes={classes} rooms={this.state.residence.rooms}/>
+                </GridContainer>
+            </div>
+        );
+    }
 }
 
 Residence.propTypes = {
-  classes: PropTypes.object.isRequired,
-  connection: PropTypes.object
+    classes: PropTypes.object.isRequired,
+    connection: PropTypes.object
 };
 
 export default withStyles(dashboardStyle)(Residence);
