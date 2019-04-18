f7ad256e51918d0ac10eccebe81f36a3beace6bf
Pedro Neto <pedromneto97@gmail.com>
2019-04-18 19:02:27 -0300

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createBrowserHistory} from "history";
-import {Redirect, Route, Router, Switch} from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { Redirect, Route, Router, Switch } from "react-router-dom";
 // core components
 import Admin from "layouts/Admin.jsx";
+import Login from "views/Login/Login.jsx";
 
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
 const hist = createBrowserHistory();
+const auth = {
+  isAuthed: false
+};
+
+function Authed(props) {
+  return (
+    <Switch>
+      <Route path="/admin" render={(props) => <Admin {...props} auth={auth}/>}/>
+      < Redirect from="/" to="/admin/dashboard"/>
+    </Switch>);
+}
+
+function NotAuthed(props) {
+  return (
+    <Switch>
+      <Route path="/login" render={(props) => <Login {...props} auth={auth}/>}/>
+      < Redirect from="/" to="/login"/>
+    </Switch>
+  );
+}
 
 ReactDOM.render(
-    <Router history={hist}>
-        <Switch>
-            <Route path="/admin" component={Admin}/>
-            <Redirect from="/" to="/admin/dashboard"/>
-        </Switch>
-    </Router>,
-    document.getElementById("root")
+  <Router history={hist}>
+    {auth.isAuthed ? <Authed/> : <NotAuthed/>}
+  </Router>,
+  document.getElementById("root")
 );
