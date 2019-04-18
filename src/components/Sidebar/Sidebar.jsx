a4010d5d84d89a0cd267fe5eaa9f1982cd2b2250
Pedro Neto <pedromneto97@gmail.com>
2019-04-18 19:05:10 -0300

    Return null of is invisible

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 // @material-ui/core components
@@ -15,129 +15,131 @@ import AdminNavbarLinks from "components/Navbars/AdminNavbarLinks.jsx";
 
 import sidebarStyle from "assets/jss/material-dashboard-react/components/sidebarStyle.jsx";
 
-const Sidebar = ({...props}) => {
-    // verifies if routeName is the one active (in browser input)
-    function activeRoute(routeName) {
-        return props.location.pathname.indexOf(routeName) > -1 ? true : false;
-    }
+const Sidebar = ({ ...props }) => {
+  // verifies if routeName is the one active (in browser input)
+  function activeRoute(routeName) {
+    return props.location.pathname.indexOf(routeName) > -1 ? true : false;
+  }
 
-    const {classes, color, logo, image, logoText, routes} = props;
-    var links = (
-        <List className={classes.list}>
-            {routes.map((prop, key) => {
-                if (!prop.invisible) {
-                    var activePro = " ";
-                    var listItemClasses;
-                    if (prop.path === "/upgrade-to-pro") {
-                        activePro = classes.activePro + " ";
-                        listItemClasses = classNames({
-                            [" " + classes[color]]: true
-                        });
-                    } else {
-                        listItemClasses = classNames({
-                            [" " + classes[color]]: activeRoute(prop.layout + prop.path)
-                        });
-                    }
-                    const whiteFontClasses = classNames({
-                        [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
-                    });
-                    return (
-                        <NavLink
-                            to={prop.layout + prop.path}
-                            className={activePro + classes.item}
-                            activeClassName="active"
-                            key={key}
-                        >
-                            <ListItem button className={classes.itemLink + listItemClasses}>
-                                {typeof prop.icon === "string" ? (
-                                    <Icon
-                                        className={classNames(classes.itemIcon, whiteFontClasses)}
-                                    >
-                                        {prop.icon}
-                                    </Icon>
-                                ) : (
-                                    <prop.icon
-                                        className={classNames(classes.itemIcon, whiteFontClasses)}
-                                    />
-                                )}
-                                <ListItemText
-                                    primary={prop.name}
-                                    className={classNames(classes.itemText, whiteFontClasses)}
-                                    disableTypography={true}
-                                />
-                            </ListItem>
-                        </NavLink>
-                    );
-                }
-            })}
-        </List>
-    );
-    var brand = (
-        <div className={classes.logo}>
-            <Link
-                to={'/'}
-                className={classNames(classes.logoLink)}
+  const { classes, color, logo, image, logoText, routes } = props;
+  let links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
+        if (!prop.invisible) {
+          var activePro = " ";
+          var listItemClasses;
+          if (prop.path === "/upgrade-to-pro") {
+            activePro = classes.activePro + " ";
+            listItemClasses = classNames({
+              [" " + classes[color]]: true
+            });
+          } else {
+            listItemClasses = classNames({
+              [" " + classes[color]]: activeRoute(prop.layout + prop.path)
+            });
+          }
+          const whiteFontClasses = classNames({
+            [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+          });
+          return (
+            <NavLink
+              to={prop.layout + prop.path}
+              className={activePro + classes.item}
+              activeClassName="active"
+              key={key}
             >
-                <div className={classes.logoImage}>
-                    <img src={logo} alt="logo" className={classes.img}/>
-                </div>
-                {logoText}
-            </Link>
+              <ListItem button className={classes.itemLink + listItemClasses}>
+                {typeof prop.icon === "string" ? (
+                  <Icon
+                    className={classNames(classes.itemIcon, whiteFontClasses)}
+                  >
+                    {prop.icon}
+                  </Icon>
+                ) : (
+                  <prop.icon
+                    className={classNames(classes.itemIcon, whiteFontClasses)}
+                  />
+                )}
+                <ListItemText
+                  primary={prop.name}
+                  className={classNames(classes.itemText, whiteFontClasses)}
+                  disableTypography={true}
+                />
+              </ListItem>
+            </NavLink>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </List>
+  );
+  var brand = (
+    <div className={classes.logo}>
+      <Link
+        to={"/"}
+        className={classNames(classes.logoLink)}
+      >
+        <div className={classes.logoImage}>
+          <img src={logo} alt="logo" className={classes.img}/>
         </div>
-    );
-    return (
-        <div>
-            <Hidden mdUp implementation="css">
-                <Drawer
-                    variant="temporary"
-                    anchor="right"
-                    open={props.open}
-                    classes={{
-                        paper: classNames(classes.drawerPaper)
-                    }}
-                    onClose={props.handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true // Better open performance on mobile.
-                    }}
-                >
-                    {brand}
-                    <div className={classes.sidebarWrapper}>
-                        <AdminNavbarLinks/>
-                        {links}
-                    </div>
-                    {image !== undefined ? (
-                        <div
-                            className={classes.background}
-                            style={{backgroundImage: "url(" + image + ")"}}
-                        />
-                    ) : null}
-                </Drawer>
-            </Hidden>
-            <Hidden smDown implementation="css">
-                <Drawer
-                    anchor="left"
-                    variant="permanent"
-                    open
-                    classes={{
-                        paper: classNames(classes.drawerPaper)
-                    }}
-                >
-                    {brand}
-                    <div className={classes.sidebarWrapper}>{links}</div>
-                    {image !== undefined ? (
-                        <div
-                            className={classes.background}
-                            style={{backgroundImage: "url(" + image + ")"}}
-                        />
-                    ) : null}
-                </Drawer>
-            </Hidden>
-        </div>
-    );
+        {logoText}
+      </Link>
+    </div>
+  );
+  return (
+    <div>
+      <Hidden mdUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor="right"
+          open={props.open}
+          classes={{
+            paper: classNames(classes.drawerPaper)
+          }}
+          onClose={props.handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>
+            <AdminNavbarLinks/>
+            {links}
+          </div>
+          {image !== undefined ? (
+            <div
+              className={classes.background}
+              style={{ backgroundImage: "url(" + image + ")" }}
+            />
+          ) : null}
+        </Drawer>
+      </Hidden>
+      <Hidden smDown implementation="css">
+        <Drawer
+          anchor="left"
+          variant="permanent"
+          open
+          classes={{
+            paper: classNames(classes.drawerPaper)
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>{links}</div>
+          {image !== undefined ? (
+            <div
+              className={classes.background}
+              style={{ backgroundImage: "url(" + image + ")" }}
+            />
+          ) : null}
+        </Drawer>
+      </Hidden>
+    </div>
+  );
 };
 
 Sidebar.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(sidebarStyle)(Sidebar);
