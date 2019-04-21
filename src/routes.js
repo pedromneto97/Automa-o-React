3709a21f692fe272452de6bb0979f41fe7b34255
Pedro Neto <pedromneto97@gmail.com>
2019-04-21 11:34:31 -0300

    Remove initial routes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
-import Person from "@material-ui/icons/Person";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import Residence from "views/Residence/Residence.jsx";
 import Room from "views/Room/Room.jsx";
-import UserProfile from "views/UserProfile/UserProfile.jsx";
-import TableList from "views/TableList/TableList.jsx";
-import Typography from "views/Typography/Typography.jsx";
-import Icons from "views/Icons/Icons.jsx";
-import Maps from "views/Maps/Maps.jsx";
-import NotificationsPage from "views/Notifications/Notifications.jsx";
 
 
 const dashboardRoutes = [
@@ -39,54 +28,6 @@ const dashboardRoutes = [
         component: Room,
         layout: "/admin",
         invisible: true
-    },
-    {
-        path: "/user",
-        name: "User Profile",
-        icon: Person,
-        component: UserProfile,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/table",
-        name: "Table List",
-        icon: "content_paste",
-        component: TableList,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/typography",
-        name: "Typography",
-        icon: LibraryBooks,
-        component: Typography,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/icons",
-        name: "Icons",
-        icon: BubbleChart,
-        component: Icons,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/maps",
-        name: "Maps",
-        icon: LocationOn,
-        component: Maps,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/notifications",
-        name: "Notifications",
-        icon: Notifications,
-        component: NotificationsPage,
-        layout: "/admin",
-        invisible: false
     }
 ];
 
