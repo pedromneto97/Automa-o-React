a2f8eee1007daf280b69e424095f16ae8be55612
Pedro Neto <pedromneto97@gmail.com>
2019-04-21 12:55:56 -0300

    Residence edit route

--- src/routes.js
+++ src/routes.js
@@ -7,28 +7,35 @@ import Room from "views/Room/Room.jsx";
 
 
 const dashboardRoutes = [
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        icon: Dashboard,
-        component: DashboardPage,
-        layout: "/admin",
-        invisible: false
-    },
-    {
-        path: "/residence/:alias",
-        name: "Residence",
-        component: Residence,
-        layout: "/admin",
-        invisible: true
-    },
-    {
-        path: "/room/:alias",
-        name: "Room",
-        component: Room,
-        layout: "/admin",
-        invisible: true
-    }
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin",
+    invisible: false
+  },
+  {
+    path: "/residence/edit/:alias",
+    name: "Residence Edit",
+    component: Residence,
+    layout: "/admin",
+    invisible: true
+  },
+  {
+    path: "/residence/:alias",
+    name: "Residence",
+    component: Residence,
+    layout: "/admin",
+    invisible: true
+  },
+  {
+    path: "/room/:alias",
+    name: "Room",
+    component: Room,
+    layout: "/admin",
+    invisible: true
+  }
 ];
 
 export default dashboardRoutes;
