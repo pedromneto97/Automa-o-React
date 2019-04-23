c40663fb518214c616f09cdc0681b9d179beca6a
pedromneto97 <pedromneto97@gmail.com>
2019-04-23 20:15:20 -0300

    style

--- src/components/Residence/ResidenceInfo.jsx
+++ src/components/Residence/ResidenceInfo.jsx
@@ -85,4 +85,35 @@ ResidenceInfo.propTypes = {
   residence: PropTypes.object.isRequired
 };
 
-export default withStyles(tasksStyle)(ResidenceInfo);
+let styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+Object.assign(styles, tasksStyle);
+
+export default withStyles(styles)(ResidenceInfo);
