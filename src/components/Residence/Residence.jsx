1c757f7f2221afa187566c67bfb53a4941a9655c
Pedro Neto <pedromneto97@gmail.com>
2019-04-30 19:57:25 -0300

    styles from Residence view

--- src/components/Residence/Residence.jsx
+++ src/components/Residence/Residence.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 // nodejs library to set properties for components
 import PropTypes from "prop-types";
 
@@ -16,53 +16,51 @@ import CardFooter from "components/Card/CardFooter.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import Tooltip from "@material-ui/core/Tooltip";
 
-import withStyles from "@material-ui/core/styles/withStyles";
-import tasksStyle from "assets/jss/material-dashboard-react/components/tasksStyle.jsx";
-
 
 class Residence extends React.Component {
-  render() {
-    const {
-      classes,
-      residence
-    } = this.props;
-    return (
-      <Card>
-        <CardHeader color="info" stats icon>
-          <CardIcon color="info">
-            <Icon>{residence.icon ? residence.icon : residence.type.icon}</Icon>
-          </CardIcon>
-          <p className={classes.cardCategory}>{residence.type.type}</p>
-          <h4 className={classes.cardTitle}>{residence.name}</h4>
-        </CardHeader>
-        <CardBody>
-          <Link to={"/admin/residence/" + residence.alias}>
-            <Button color="info" size="sm">Show Rooms</Button>
-          </Link>
-          <Link to={"/admin/residence/info/" + residence.alias}>
-            <Tooltip
-              id="tooltip-top"
-              title="Residence Info"
-              placement="top"
-              classes={{ tooltip: classes.tooltip }}>
-              <Button justIcon round color="secondary" size='sm' className='pull-right'><Settings/></Button>
-            </Tooltip>
-          </Link>
-        </CardBody>
-        <CardFooter chart>
-          <div className={classes.stats}>
-            <LocationCity/>
-            {residence.address.street}, {residence.address.number}, {residence.address.district} {residence.address.postal_code.city ? "- " + residence.address.postal_code.city : ""}
-          </div>
-        </CardFooter>
-      </Card>
-    );
-  }
+    render() {
+        const {
+            classes,
+            residence
+        } = this.props;
+        return (
+            <Card>
+                <CardHeader color="info" stats icon>
+                    <CardIcon color="info">
+                        <Icon>{residence.icon ? residence.icon : residence.type.icon}</Icon>
+                    </CardIcon>
+                    <p className={classes.cardCategory}>{residence.type.type}</p>
+                    <h4 className={classes.cardTitle}>{residence.name}</h4>
+                </CardHeader>
+                <CardBody>
+                    <Link to={"/admin/residence/" + residence.alias}>
+                        <Button color="info" size="sm">Show Rooms</Button>
+                    </Link>
+                    <Link to={"/admin/residence/info/" + residence.alias}>
+                        <Tooltip
+                            id="tooltip-top"
+                            title="Residence Info"
+                            placement="top"
+                            classes={{tooltip: classes.tooltip}}>
+                            <Button justIcon round color="secondary" size='sm'
+                                    className='pull-right'><Settings/></Button>
+                        </Tooltip>
+                    </Link>
+                </CardBody>
+                <CardFooter chart>
+                    <div className={classes.stats}>
+                        <LocationCity/>
+                        {residence.address.street}, {residence.address.number}, {residence.address.district} {residence.address.postal_code.city ? "- " + residence.address.postal_code.city : ""}
+                    </div>
+                </CardFooter>
+            </Card>
+        );
+    }
 }
 
 Residence.propTypes = {
-  classes: PropTypes.object.isRequired,
-  residence: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    residence: PropTypes.object.isRequired
 };
 
-export default withStyles(tasksStyle)(Residence);
+export default Residence;
