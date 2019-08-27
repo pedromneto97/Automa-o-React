43bdce32d312e57a7beef61f793ba62b77160cc2
Pedro Neto <pedromneto97@gmail.com>
2019-08-26 22:44:45 -0300

    Add: Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducers from "./reducers";
+import {asyncDispatchMiddleware} from "./middlewares";
 
-export default createStore(reducers);
+export default createStore(reducers, applyMiddleware(asyncDispatchMiddleware));
