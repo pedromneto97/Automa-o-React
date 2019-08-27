61f0c0b7f4bb3897fb4a478b0c551d22340c602b
Pedro Neto <pedromneto97@gmail.com>
2019-08-26 22:46:30 -0300

    Update: Unifica master mudança de estado do crossbar

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,25 +1,8 @@
 import {combineReducers} from "redux";
 
-import {ADD_ON_OPEN, ADD_RESIDENCE, ADD_SESSION, REMOVE_ON_OPEN} from "../actions/actionTypes";
-
-function on_open(state = [], action) {
-    switch (action.type) {
-        case ADD_ON_OPEN:
-            return [
-                ...state,
-                {
-                    topic: action.topic,
-                    callback: action.callback
-                }
-            ];
-        case REMOVE_ON_OPEN:
-            return state.filter(function (item) {
-                return item !== action.topic;
-            });
-        default:
-            return state;
-    }
-}
+import {ADD_ON_OPEN, ADD_RESIDENCE, ADD_SESSION, OPEN_CONNECTION, REMOVE_ON_OPEN} from "../actions/actionTypes";
+import {auth_cra, Connection} from "autobahn";
+import {add_session} from "../actions";
 
 function residence(state = {}, action) {
     if (action.type === ADD_RESIDENCE) {
@@ -28,15 +11,81 @@ function residence(state = {}, action) {
     return state;
 }
 
-function session(state = null, action) {
-    if (action.type === ADD_SESSION) {
-        return action.session;
+function crossbar(state = {
+    connection: null,
+    session: null,
+    on_open: []
+}, action) {
+    switch (action.type) {
+        case ADD_ON_OPEN:
+            return {
+                on_open: [
+                    ...state.on_open,
+                    {
+                        topic: action.topic,
+                        callback: action.callback
+                    }
+                ],
+                ...state
+            };
+        case REMOVE_ON_OPEN:
+            return {
+                on_open: state.on_open.filter(function (item) {
+                    return item !== action.topic;
+                }),
+                ...state
+            };
+        case OPEN_CONNECTION:
+            const ws_uri = {};
+            if (document.location.hostname === "localhost") {
+                ws_uri.transports = [
+                    {
+                        type: "websocket",
+                        url: "ws://crossbar-pedro.herokuapp.com/ws"
+                    }
+                ];
+            } else {
+                ws_uri.url = (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" + document.location.host + "/ws";
+            }
+            ws_uri["realm"] = "realm1";
+            ws_uri["authmethods"] = ["wampcra"];
+            ws_uri["authid"] = action.username;
+            ws_uri["onchallenge"] = function (session, method, extra) {
+                if (method === "wampcra") {
+                    let key;
+                    if ("salt" in extra) {
+                        key = auth_cra.derive_key(action.password, extra.salt, extra.iterations, extra.keylength);
+                    } else {
+                        key = action.password;
+                    }
+                    return auth_cra.sign(key, extra.challenge);
+                }
+            };
+
+            const connection = new Connection(ws_uri);
+            connection.onopen = function (session) {
+                console.info("Aberto");
+                state.on_open.forEach(item => {
+                    session.subscribe(item.topic, item.callback);
+                });
+                action.asyncDispatch(add_session(session));
+            };
+            connection.open();
+            return {
+                ...state,
+                connection
+            };
+        case ADD_SESSION:
+            return {
+                ...state,
+                session: action.session
+            };
+        default:
+            return state;
     }
-    return state;
 }
 
 export default combineReducers({
-    crossbar: on_open,
-    session,
+    crossbar,
     residence
 });
